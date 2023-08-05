4c627cb611b199e14b435ec490f284b9bef4025e
Jadon Gearhart <jadong@Jadons-MacBook-Air.local>
2023-08-05 11:53:05 -0400

    caching?%

--- static/service-worke.js
+++ static/service-worke.js
@@ -1,15 +1,31 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        // Clone the response to store in cache
-        const clonedResponse = response.clone();
-        caches.open('my-site-cache-v1')
-          .then((cache) => cache.put(event.request, clonedResponse));
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+self.addEventListener('fetch', function (event) {
+
+  // Get the request
+  let request = event.request;
+
+  // Bug fix
+  // https://stackoverflow.com/a/49719964
+  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+
+  // HTML files
+  // Network-first
+  if (request.headers.get('Accept').includes('text/html')) {
+    // Handle HTML files...
+    return;
+  }
+
+  // CSS & JavaScript
+  // Offline-first
+  if (request.headers.get('Accept').includes('text/css') || request.headers.get('Accept').includes('text/javascript')) {
+    // Handle CSS and JavaScript files...
+    return;
+  }
+
+  // Images
+  // Offline-first
+  if (request.headers.get('Accept').includes('image')) {
+    // Handle images...
+  }
+
 });
+
