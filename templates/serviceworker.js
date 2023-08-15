55186106f78a8614efeace6564e6c5c977f1ea2f
Jadon Gearhart <jadong@Jadons-MacBook-Air.local>
2023-08-15 18:38:46 -0400

    Maybe..

--- templates/serviceworker.js
+++ templates/serviceworker.js
@@ -1,4 +1,3 @@
-// Name of the cache
 const CACHE_NAME = 'my-app-cache-v1';
 
 // Files to cache
@@ -27,42 +26,45 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
+  if (isExcludedUrl(event.request.url)) {
+    // Exclude the URL from caching
+    return fetch(event.request);
+  }
+
   event.respondWith(
-    isExcludedUrl(event.request.url) ?
-      fetch(event.request) :
-      caches.match(event.request)
-        .then(function (response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request).then(
-            function (response) {
-              // Check if we received a valid response
-              if (!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
-
-              caches.open(CACHE_NAME)
-                .then(function (cache) {
-                  cache.put(event.request, responseToCache);
-                });
-
+        return fetch(event.request).then(
+          function (response) {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-          );
-        })
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
   );
 });
 
 self.addEventListener('activate', function (event) {
-
   var cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
@@ -80,10 +82,5 @@ self.addEventListener('activate', function (event) {
 
 // Utility function to check if a URL is in the excluded URLs list
 function isExcludedUrl(url) {
-  for (let i = 0; i < excludedUrls.length; i++) {
-    if (url === excludedUrls[i] || url.startsWith(excludedUrls[i] + '/')) {
-      return true;
-    }
-  }
-  return false;
+  return excludedUrls.includes(url);
 }
