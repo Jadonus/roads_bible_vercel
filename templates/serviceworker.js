3bd1d7a4e589ac54dcde47221b3d0750958fbd3b
Jadon Gearhart <jadong@Jadons-MacBook-Air.local>
2023-08-15 15:57:01 -0400

    IM TRYING OK PLEASE WORK

--- templates/serviceworker.js
+++ templates/serviceworker.js
@@ -6,58 +6,69 @@ const urlsToCache = [
   // Add more files and routes as needed
 ];
 
-self.addEventListener('install', function(event) {
+// URLs to exclude from caching
+const excludedUrls = [
+  '/dashboard',
+  '/accounts/login',
+  '/accounts/logout',
+  '/accounts/signup',
+  '/'
+];
+
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
+    isExcludedUrl(event.request.url) ?
+      fetch(event.request) :
+      caches.match(event.request)
+        .then(function (response) {
+          // Cache hit - return response
+          if (response) {
             return response;
           }
-        );
-      })
-    );
+          return fetch(event.request).then(
+            function (response) {
+              // Check if we received a valid response
+              if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then(function (cache) {
+                  cache.put(event.request, responseToCache);
+                });
+
+              return response;
+            }
+          );
+        })
+  );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
 
   var cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
@@ -67,3 +78,12 @@ self.addEventListener('activate', function(event) {
   );
 });
 
+// Utility function to check if a URL is in the excluded URLs list
+function isExcludedUrl(url) {
+  for (let i = 0; i < excludedUrls.length; i++) {
+    if (url === excludedUrls[i] || url.startsWith(excludedUrls[i] + '/')) {
+      return true;
+    }
+  }
+  return false;
+}
