ae9d1a3d4a76c0fbd2f400995196406c55a5d501
Jadon Gearhart <jadong@Jadons-MacBook-Air.local>
2023-08-15 18:51:43 -0400

    workbox

--- templates/serviceworker.js
+++ templates/serviceworker.js
@@ -1,86 +1,33 @@
-const CACHE_NAME = 'my-app-cache-v1';
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
-// Files to cache
-const urlsToCache = [
-  // Add more files and routes as needed
-];
+const CACHE_NAME = 'my-app-cache-v1';
 
 // URLs to exclude from caching
 const excludedUrls = [
-  '/dashboard',
-  '/accounts/login',
-  '/accounts/logout',
-  '/accounts/signup',
-  '/'
+  '/dashboard/',
+  '/accounts/login/',
+  '/accounts/logout/',
+  '/accounts/signup/',
 ];
 
-self.addEventListener('install', function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened custom cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// Configure Workbox to use the cache name
+workbox.core.setCacheNameDetails({ prefix: CACHE_NAME });
 
-self.addEventListener('fetch', function (event) {
-  if (isExcludedUrl(event.request.url)) {
-    // Exclude the URL from caching
-    return fetch(event.request);
-  }
+// Exclude specific URLs from caching
+workbox.routing.registerRoute(
+  ({ url }) => isExcludedUrl(url.pathname),
+  new workbox.strategies.NetworkOnly()
+);
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function (response) {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function (cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-  );
-});
-
-self.addEventListener('activate', function (event) {
-  var cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+// Cache other requests using the Stale-While-Revalidate strategy
+workbox.routing.registerRoute(
+  ({ url }) => !isExcludedUrl(url.pathname),
+  new workbox.strategies.StaleWhileRevalidate()
+);
 
 // Utility function to check if a URL is in the excluded URLs list
 function isExcludedUrl(url) {
-  return excludedUrls.includes(url);
+  return excludedUrls.some(function (excludedUrl) {
+    return url.startsWith(excludedUrl);
+  });
 }
