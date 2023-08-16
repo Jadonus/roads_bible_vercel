f062e4542442652ec41e1b08090747b6b9c86278
Jadon Gearhart <jadong@Jadons-MacBook-Air.local>
2023-08-16 16:41:15 -0400

    will it work?

--- road_bible/settings.py
+++ road_bible/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&x$2rzcqf5w_jkx(zzz+u=99$(f^l^l*hw7f^wgi5cajc$92jp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["192.168.200.185", "roadsbible.vercel.app", "192.168.207.164", "www.roadsbible.com"]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False 
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jadongearhart@gmail.com' 
 EMAIL_HOST_PASSWORD = 'kmolelwgnxvxwiqr'
--- templates/serviceworker.js
+++ templates/serviceworker.js
@@ -1,33 +1,12 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
-
-const CACHE_NAME = 'my-app-cache-v2';
-
-// URLs to exclude from caching
-const excludedUrls = [
-  '{% url "/" %}',
-  '/dashboard/',
-  '/accounts/login/',
-  '/accounts/logout/',
-  '/accounts/signup/',
-];
-
-// Configure Workbox to use the cache name
-workbox.core.setCacheNameDetails({ prefix: CACHE_NAME });
-
-// Exclude specific URLs from caching
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 workbox.routing.registerRoute(
-  ({ url }) => isExcludedUrl(url.pathname),
-  new workbox.strategies.NetworkOnly()
+  ({url}) => url.origin === 'https://example.com',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'roadscache1.0',
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  })
 );
-
-// Cache other requests using the Stale-While-Revalidate strategy
-workbox.routing.registerRoute(
-  ({ url }) => !isExcludedUrl(url.pathname),
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-function isExcludedUrl(url) {
-  return excludedUrls.some(function (excludedUrl) {
-    return url === excludedUrl;
-  });
-}
