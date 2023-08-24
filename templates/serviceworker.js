6f05569624c053c30c1cdfee7195a5aa14b47107
jadonus <jadongearhart@gmail.com>
2023-08-24 11:26:49 -0400

    service worker fix

--- templates/serviceworker.js
+++ templates/serviceworker.js
@@ -11,9 +11,9 @@ workbox.precaching.precacheAndRoute([]);
 
 // Cache routes starting with /roads and /static
 workbox.routing.registerRoute(
-  new RegExp('/(roads|static)/'),
+  new RegExp('/static'),
   new workbox.strategies.CacheFirst({
-    cacheName: 'my-cache', // Change this to a unique name
+    cacheName: 'my-cache2', // Change this to a unique name
     plugins: [
       new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200], // Cache responses with these status codes
