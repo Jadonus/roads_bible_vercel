f5e83c712ada894c07344416ae983170a980469f
jadonus <jadongearhart@gmail.com>
2023-08-24 11:42:48 -0400

    service worker fix 2.0

--- templates/serviceworker.js
+++ templates/serviceworker.js
@@ -1,6 +1,4 @@
-// Import Workbox from CDN
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
-
 // Set up Workbox
 workbox.setConfig({
   debug: true, // Enable logging
@@ -9,11 +7,11 @@ workbox.setConfig({
 // Precache your assets (optional)
 workbox.precaching.precacheAndRoute([]);
 
-// Cache routes starting with /roads and /static
+// Use StaleWhileRevalidate strategy for caching
 workbox.routing.registerRoute(
   new RegExp('/static'),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'my-cache2', // Change this to a unique name
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'my-cache-2', // Change this to a unique name
     plugins: [
       new workbox.cacheableResponse.CacheableResponsePlugin({
         statuses: [0, 200], // Cache responses with these status codes
