8b70d2b272b4eab7865533f2a4848feaae97bd77
jadonus <jadongearhart@gmail.com>
2023-10-25 09:39:49 -0400

    SETI

--- road_bible/settings.py
+++ road_bible/settings.py
@@ -202,3 +202,11 @@ PWA_APP_SCREENSHOTS = [
         "type": "image/png"
     }
 ]
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
+}
